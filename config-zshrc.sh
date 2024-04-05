7a1c068666fc4e1e262356f7c426c03d0d048d04
dean glükler <glueckler@gmail.com>
2024-04-04 23:59:57 -0600

    Add zsh configuration script

---
+++ config-zshrc.sh
@@ -0,0 +1,18 @@
+DIRECTOR="$HOME/.config/zsh"
+ZSHRC="$HOME/.config/zsh/.zshrc"
+
+mkdir -p "$HOME/.config"
+
+read -p "REPLACING $DIRECTOR - Are you suuure zsh is backed up?"$'\n'"-> Enter to continue, or Ctrl+C to cancel"
+
+if [ -d "$DIRECTOR" ]; then
+    echo "Deleting $DIRECTOR"
+    rm -rf "$DIRECTOR"
+fi
+
+git clone git@github.com:deanglukler/zsh.git $DIRECTOR
+
+if [ ! -L "$HOME/.zshrc" ]; then
+    echo "Linking $ZSHRC to $HOME/.zshrc"
+    ln -s "$ZSHRC" "$HOME/.zshrc"
+fi
